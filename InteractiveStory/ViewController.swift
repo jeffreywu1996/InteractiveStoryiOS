b36873a3e6fb0b5fb920adb60e93277a8401a615
Jeffrey Wu <jeffreywu1996@gmail.com>
2016-04-16 15:14:24 -0700

    Initial Commit

---
+++ InteractiveStory/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InteractiveStory
+//
+//  Created by Jeffrey Wu on 4/16/16.
+//  Copyright © 2016 Jeffrey Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
