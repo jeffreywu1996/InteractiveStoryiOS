1e5316d1500c5960120266f018d461c87fc98660
Jeffrey Wu <jeffreywu1996@gmail.com>
2016-04-18 16:16:15 -0700

    Added picture files

---
+++ PageContoller.swift
@@ -0,0 +1,56 @@
+//
+//  PageContoller.swift
+//  InteractiveStory
+//
+//  Created by Jeffrey Wu on 4/16/16.
+//  Copyright © 2016 Jeffrey Wu. All rights reserved.
+//
+
+import UIKit
+
+class PageContoller: UIViewController {
+    
+    var page: Page?
+    
+    let artwork = UIImageView()
+    let storyLabel = UILabel()
+    let firstChoiceButton = UIButton(type: .System)
+    let secondChoiceButton = UIButton(type: .System)
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    init(page: Page) {
+        self.page = page
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .blueColor()
+        if let page = page {
+            print(page.story.text)
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
